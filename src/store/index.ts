cd5d1bb0465e54627f860710d4bfb45bf109ee5b
anikushalam <rjsingh.ak@gmail.com>
2024-11-29 14:13:36 +0530

    with id empyt

--- src/store/index.ts
+++ src/store/index.ts
@@ -35,13 +35,14 @@ export interface State {
 export const useStore = create<State>((set) => ({
   insName: undefined,
   name: undefined,
-  id: "",
   // id: "651ba22de39dbdf817dd520c",
   // id: "660bd1c7d5016c9947aef713",
   // rs sapat
   // id: "667ac2be2b6329957435b217",
   // pvg
   // id: "6660214803ff919dc4b26877",
+  // nes/
+  id: "65c48434896ca745f41b7381",
 
   color: undefined,
   status: undefined,
